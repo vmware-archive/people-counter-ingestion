0197741b10aad56b54a4a1f8e981c342beb0c1e9
Luis M. Valerio Castillo <lvaleriocasti@vmware.com>
2019-08-22 17:47:13 -0500

    Updated to use the correct method for continues image capturing

--- image_capture_daemon.py
+++ image_capture_daemon.py
@@ -1,12 +1,21 @@
 import time
+from time import sleep
+from picamera import PiCamera
 
 class App():
     def __init__(self):
         self.image_capture_folder = '/tmp'
     def run(self):
         while True:
-            print("Howdy!  Gig'em!  Whoop!")
-            time.sleep(10)
+            camera = PiCamera()
+            camera.resolution = (1024, 768)
+            camera.start_preview()
+            sleep(10)
+            print('Capturing images to folder ' + self.image_capture_folder + '...')
+            for filename in camera.capture_continuous(self.image_capture_folder + '/img{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
+                print('Captured %s' % filename)
+                # Sleep 10 seconds before taking next picture
+                sleep(10)
 
 app = App()
 app.run()
