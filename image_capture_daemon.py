7f8ef2942ab821751030fa17c14e801aad1005bc
Luis M. Valerio Castillo <lvaleriocasti@vmware.com>
2019-08-26 15:50:45 -0500

    Added image directory clean up function

--- image_capture_daemon.py
+++ image_capture_daemon.py
@@ -9,6 +9,8 @@ class App():
         # Initialization function which parses command-line arguments from the user as well as set defaults
 
         # Default values
+        self.image_cache_size = 10
+        self.image_cleanup_interval_mintues = 1
         self.camera_warmup_delay = 2
         image_storage_folder_default = '/tmp'
         image_resolution_default = [1024, 768]
@@ -42,8 +44,37 @@ class App():
             raise Exception('The folder {0} specified for image storage is not writtable'.format(self.args.image_storage_folder))
         if self.args.image_resolution[0] > 2592 or self.args.image_resolution[1] > 1944:
             raise Exception('The resolution provided {0} exceeds the max allowed resolution 2592x1944 for the camera'.format(self.args.image_resolution))
-        if '{counter}' not in self.args.image_filename_format and '{timestamp}' not in self.args.image_filename_format:
-            raise Exception('The image file format provided did not contain {counter} or {timestamp} in it')
+        if '{counter' not in self.args.image_filename_format and '{timestamp' not in self.args.image_filename_format:
+            raise Exception('The image file format provided {0} did not contain {{counter}} or {{timestamp}} in it'.format(self.args.image_filename_format))
+
+    def cleanImageStorageDirectory(self):
+        # This function cleans up the directory where images are stored based on a limit on a number of images to keep defined by the user
+
+        filenames = os.listdir(self.args.image_storage_folder)
+        full_file_paths = []
+        search_criteria = self.args.image_filename_format.split('.')
+        search_criteria = search_criteria[len(search_criteria) - 1]
+        print("Looking for files with extention {0}".format(search_criteria))
+
+        # Find all the files with a particualar extention
+        for filename in filenames:
+            if search_criteria in filename:
+                full_file_paths.append(os.path.join(self.args.image_storage_folder, filename))
+
+        # Exit the function if no images are present
+        file_list_size = len(full_file_paths)
+        if file_list_size <= self.image_cache_size:
+            print("Image storage folder has not exceeded the max number of files allowed: {0}. No clean up performed".format(str(self.image_cache_size)))
+            return
+        
+        # Delete all images past the max number of images allowed. Oldest files are deleted first.
+        if file_list_size > 0:
+            full_file_paths.sort(key=os.path.getctime)
+            print(full_file_paths)
+            number_of_items_to_delete = file_list_size - self.image_cache_size
+            for i in range(number_of_items_to_delete):
+                print("Deleting file: {0}".format(full_file_paths[i]))
+                os.remove(full_file_paths[i])
 
     def run(self):
         while True:
@@ -56,6 +87,7 @@ class App():
                 print('Captured %s' % filename)
                 # Sleep 10 seconds before taking next picture
                 sleep(self.args.image_capture_interval)
+                self.cleanImageStorageDirectory()
 
 app = App()
 app.run()
