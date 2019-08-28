d7832f80e8ed1922a6086f1d9e5522a894771897
Luis M. Valerio Castillo <lvaleriocasti@vmware.com>
2019-08-28 14:39:48 -0500

    Updated setup script to use the install command and to add the user to the video group in order to use the camera

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 read -sp 'Enter password to create iotadmin user:' userpass
 echo
-useradd -p $(echo $userpass | openssl passwd -1 -stdin) iotadmin
+useradd -p $(echo $userpass | openssl passwd -1 -stdin) -g video iotadmin
 if [ $? -eq 0 ]; then
     echo "User iotadmin added successfully"
 else
@@ -12,4 +12,4 @@ fi
 
 echo "Copying python code to /opt/vmware/people-counter-ingestion-service..."
 mkdir -p /opt/vmware/people-counter-ingestion-service
-cp image_capture_daemon.py /opt/vmware/people-counter-ingestion-service/
+install -C -m 775 -o iotadmin -g iotadmin image_capture_daemon.py /opt/vmware/people-counter-ingestion-service
