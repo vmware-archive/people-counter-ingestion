f17572b1400b97205f049c77f5eeb48a1f791085
Luis M. Valerio Castillo <lvaleriocasti@vmware.com>
2019-08-28 14:15:32 -0500

    Added section in bash script to create operations directory

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-read -sp 'Enter password to create iotadmin user:\n' userpass
-
+read -sp 'Enter password to create iotadmin user:' userpass
+echo
 useradd -p $(echo $userpass | openssl passwd -1 -stdin) iotadmin
 if [ $? -eq 0 ]; then
     echo "User iotadmin added successfully"
 else
     echo "Failed to create iotadmin user"
+    exit 1
 fi
 
+echo "Copying python code to /opt/vmware/people-counter-ingestion-service..."
+mkdir -p /opt/vmware/people-counter-ingestion-service
+cp image_capture_daemon.py /opt/vmware/people-counter-ingestion-service/
