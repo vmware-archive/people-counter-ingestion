7cf936b3fd80c6ac8491545aa5e95126d570d527
Luis M. Valerio Castillo <lvaleriocasti@vmware.com>
2019-09-04 20:00:31 -0500

    Updated setup script to use the WORKDIR variable from Pulse

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
+if [ -z "$DATADIR" ]
+then
+    DATADIR=pwd
+fi
+
 # Check if a user exists. Create it if it does not.
 if id iotadmin > /dev/null 2>&1; then
     echo "iotadmin user already exists. Will not create."
@@ -20,7 +25,7 @@ fi
 
 echo "Copying python code to /opt/vmware/people-counter-ingestion-service..."
 mkdir -p /opt/vmware/people-counter-ingestion-service
-install -C -m 775 -o iotadmin -g video image_capture_daemon.py /opt/vmware/people-counter-ingestion-service
+install -C -m 775 -o iotadmin -g video $DATADIR/image_capture_daemon.py /opt/vmware/people-counter-ingestion-service
 
 echo "Copying unit file to /etc/systemd/system/...."
-install -C -m 775 -o root -g root people-counter-ingestion.service /etc/systemd/system
+install -C -m 775 -o root -g root $DATADIR/people-counter-ingestion.service /etc/systemd/system
