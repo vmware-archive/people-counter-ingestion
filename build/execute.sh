f1c9525f6a402d57d7d2e364216e6ce95189a206
Luis M. Valerio Castillo <lvaleriocasti@vmware.com>
2019-09-04 19:24:16 -0500

    Added execute script

---
+++ build/execute.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Copyright (c) 2019 VMware, Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+# Start the service and enable it to restart when the machine boots up
+sudo ./setup.sh
