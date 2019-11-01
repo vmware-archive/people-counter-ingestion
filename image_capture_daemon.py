ca50746170f45ec493368a754b136a0119beb182
Luis M. Valerio Castillo <lvaleriocasti@vmware.com>
2019-11-01 17:05:03 -0500

    Added username and password to connection info for MQTT

--- image_capture_daemon.py
+++ image_capture_daemon.py
@@ -223,9 +223,9 @@ class App():
 
     def run(self):
         # Initialize the MQTT client
-        #self.mqtt_client.username_pw_set(self.args.mqtt_username, self.mqtt_client.mqtt_password)
         global mqtt_client_connection_error
         logging.info("Connecting to MQTT broker...")
+        self.mqtt_client.username_pw_set(self.args.mqtt_username, self.mqtt_client.mqtt_password)
         self.mqtt_client.connect(self.args.mqtt_hostname, self.args.mqtt_port)
         self.mqtt_client.loop_start()
         sleep(10)
