fe90bc5dbadaa7911068eec983540079001c0004
Luis M. Valerio Castillo <lvaleriocasti@vmware.com>
2019-11-13 17:23:38 -0600

    Fixed issues in the unit file and script to keep the IP address up to date

--- configure/ansible-role-image-ingestion-service/roles/image-ingestion-service/files/ip-address-manager.sh
+++ configure/ansible-role-image-ingestion-service/roles/image-ingestion-service/files/ip-address-manager.sh
@@ -5,4 +5,4 @@ ipaddress=$(hostname -I | awk '{print $1}')
 echo The IP address of this machine is $ipaddress | systemd-cat -t ingestion-service-install -p info
 
 # Take the IP address and replace the one in /etc/hosts
-sed -i -e "s/.*raspberrypi$/$ipaddress     raspberrypi/g" test.txt
+sed -i -e "s/.*raspberrypi$/$ipaddress     raspberrypi/g" /etc/hosts
