7898686e48526f4b4400734dfce4e2f4cb90ef8c
Luis M. Valerio Castillo <lvaleriocasti@vmware.com>
2019-11-13 17:36:34 -0600

    Fixed log message topic

--- configure/ansible-role-image-ingestion-service/roles/image-ingestion-service/files/ip-address-manager.sh
+++ configure/ansible-role-image-ingestion-service/roles/image-ingestion-service/files/ip-address-manager.sh
@@ -2,7 +2,7 @@
 
 # Find the IP address of the machine
 ipaddress=$(hostname -I | awk '{print $1}')
-echo The IP address of this machine is $ipaddress | systemd-cat -t ingestion-service-install -p info
+echo The IP address of this machine is $ipaddress | systemd-cat -t ip-address-manager -p info
 
 # Take the IP address and replace the one in /etc/hosts
 sed -i -e "s/.*raspberrypi$/$ipaddress     raspberrypi/g" /etc/hosts
